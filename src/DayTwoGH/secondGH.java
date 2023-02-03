7e4833c82b901ae8c9ee4f2f654a4bd6a69ecc76
aselshaibekova <7aselshaibekova@gmail.com>
2023-02-03 18:25:36 +0600

    some changes

--- src/DayTwoGH/secondGH.java
+++ src/DayTwoGH/secondGH.java
@@ -3,5 +3,6 @@ package DayTwoGH;
 public class secondGH {
     public static void main(String[] args) {
         System.out.println("Hey ppl im in github");
+        System.out.println(2+2);
     }
 }
